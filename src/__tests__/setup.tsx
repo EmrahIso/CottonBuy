db80dbab0ad60f8062cc3b92bc54bcf81a87a050
Emrah Isovic <emrahisovic20@gmail.com>
2025-09-27 13:48:48 +0200

    Added vitest and RTL for testing

---
+++ src/__tests__/setup.tsx
@@ -0,0 +1,13 @@
+import { expect, afterEach } from 'vitest';
+import { cleanup } from '@testing-library/react';
+import * as matchers from '@testing-library/jest-dom/matchers';
+
+import '@testing-library/jest-dom';
+
+expect.extend(matchers);
+
+afterEach(() => {
+  cleanup();
+});
+
+export * from '@testing-library/react';
